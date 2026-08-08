49344146df18a66f196fac597e2425005ac62038
PranavThaker <thakerpranav9@gmail.com>
2026-08-08 19:40:55 +0530

    Configure production CORS

--- backend/app.py
+++ backend/app.py
@@ -25,7 +25,8 @@ app = FastAPI(title="Laptop Price Prediction API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://laptop-price-prediction-five.vercel.app/"
     ],
     allow_credentials=False,
     allow_methods=["*"],
